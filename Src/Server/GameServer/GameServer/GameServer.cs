6370a80a8982b8f0591af01ba484e89db6a15cb8
ray <ray@raymix.net>
2018-11-14 19:05:15 +0800

    add lesson 11

--- Src/Server/GameServer/GameServer/GameServer.cs
+++ Src/Server/GameServer/GameServer/GameServer.cs
@@ -5,27 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using GameServer.Network;
 using System.Configuration;
 
 using System.Threading;
 
 using Network;
 using GameServer.Services;
-
+using GameServer.Managers;
 namespace GameServer
 {
     class GameServer
     {
+        NetService network;
         Thread thread;
         bool running = false;
-        NetService network;
-
         public bool Init()
         {
             int Port = Properties.Settings.Default.ServerPort;
             network = new NetService();
             network.Init(Port);
             DBService.Instance.Init();
+            DataManager.Instance.Load();
+            MapService.Instance.Init();
             UserService.Instance.Init();
             thread = new Thread(new ThreadStart(this.Update));
 
